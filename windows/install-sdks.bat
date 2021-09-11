714c6be3539b8c1e7f5381911d5b809a5ec6f1c3
Shengyou Fan <shengyoufan@gmail.com>
2021-09-11 21:03:45 +0800

    update Windows SDKs list

--- windows/install-sdks.bat
+++ windows/install-sdks.bat
@@ -1,9 +1,20 @@
 :: SDKs installer for Windows 10
 
+:: Git
+scoop install git
+
+:: Improve download speed
+scoop install aria2
+
+:: Compress
+scoop install 7zip
+
 :: Tools
-scoop install git curl
+scoop install curl wget
+
+:: ffmpeg
+scoop install ffmpeg
 
 :: JDKs
 scoop bucket add java
 scoop install openjdk
-
