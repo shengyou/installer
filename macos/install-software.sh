8cdfbcb225ccf533391688633ef66d25f1cebf26
Shengyou Fan <shengyoufan@gmail.com>
2024-06-05 22:10:57 +0800

    Update software list on macOS

--- macos/install-software.sh
+++ macos/install-software.sh
@@ -1,38 +1,38 @@
 # Software installer for macOS
 
 # install mas-cli first
-brew install mas
-mas signin "<Apple ID>" # it's broken
+# brew install mas
+# mas signin "<Apple ID>" # it's broken
 
-# iTerm and Alfred first, PLEASE
-#brew install --cask iterm2
-#brew install --cask alfred
+# Termianl and Launcher first, PLEASE
 brew install --cask warp
 brew install --cask raycast
 
 # Browsers
 brew install --cask google-chrome
 brew install --cask firefox
-brew install --cask brave-browser
+brew install --cask arc
+# brew install --cask brave-browser
 
 # IMs
 mas install 539883307 # LINE
-mas install 747648890 # Telegram
-mas install 803453959 # Slack for Desktop
+brew install --cask telegram # mas install 747648890 # Telegram
+brew install --cask slack # mas install 803453959 # Slack for Desktop
+brew install --cask whatsapp
+brew install --cask signal
 brew install --cask discord
 brew install --cask microsoft-teams
-brew install --cask whatsapp
 brew install --cask skype
 mas install 836500024 # WeChat
-mas install 1435447041 # 钉钉 DingTalk
 mas install 451108668 # 腾讯 QQ
+mas install 1435447041 # 钉钉 DingTalk
 
 # Meetings
 brew install --cask zoom
 brew install --cask tencent-meeting
-#mas install 1497685373 # VooV Meeting
-#brew install --cask teamviewer
-#brew install --cask webex
+# mas install 1497685373 # VooV Meeting
+# brew install --cask teamviewer
+# brew install --cask webex
 
 # Multimedia
 brew install --cask spotify
@@ -49,16 +49,15 @@ brew install --cask streamlabs-obs
 # Video Editing
 mas install 571213070 # DaVinci Resolve
 
-:: Converting
+# Converting
 brew install --cask handbrake
 
 # Life
-brew install --cask nordvpn
-brew install --cask toggl-track
 brew install --cask 1password
 brew install --cask notion
-brew install --cask anki
 brew install --cask keka
+brew install --cask toggl-track
+brew install --cask anki
 
 # Network disc
 brew install --cask google-drive
@@ -68,10 +67,13 @@ brew install --cask dropbox
 brew install --cask docker
 
 # Development tools
-mas install 497799835 # Xcode
+brew install aria2
+brew install xcodes # mas install 497799835 # Xcode
+xcodes list # get versions
+xcodes install <version>
 brew install --cask jetbrains-toolbox
-brew install --cask mamp
 brew install --cask visual-studio-code
+brew install --cask mamp
 brew install --cask virtualbox
 brew install --cask vagrant
 brew install --cask tunnelblick
